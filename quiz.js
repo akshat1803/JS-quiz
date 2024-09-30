d2744d337b3cc10970189012ad7e92109c3e3a9b
akshat1803 <sakshat678@gmail.com>
2024-09-30 14:26:15 +0530

    modified

--- quiz.js
+++ quiz.js
@@ -1,19 +1,4 @@
-const questions = [
-  { q: "2+2", opt: [1, 2, 3, 4], correct: 4, hasImage: false },
-
-  { q: "2*2*2-8", opt: [8, 0, -2, 1], correct: 0, hasImage: false },
-
-  { q: "3+3+3", opt: [0, 333, 6, 9], correct: 9, hasImage: false },
-
-  { q: "4*4/4", opt: [4, 0, 8, 16], correct: 4, hasImage: false },
-
-  {
-    q: '<img class="photo" src="assets/ram mandir.webp" /> <br> Which monument is this?',
-    opt: ["Taj Mahal", "Red Fort", "Qutub Minar", "Ram Mandir"],
-    correct: "Ram Mandir",
-    hasImage: true,
-  },
-]; 
+import { questions } from "./question.js"; 
 
 const para = document.querySelector(".question");
 const optionsPara = document.querySelectorAll("#options p");
@@ -26,7 +11,7 @@ const temp = [];
 let didUserAnswer = false;
 
 let i = 0;
-let timer = 5;
+let timer = 5; 
 
 // GENERATE A RANDOM ORDER ON PAGE LOAD
 for (let i = 0; i < questions.length; i++) {
